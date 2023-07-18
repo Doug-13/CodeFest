7a542a59af916f12f8b5b03baeeec9e5b0367d96
Doug-13 <douglasazevedomello2@gmail.com>
2023-07-18 08:04:27 -0300

    first commit

---
+++ codeFest-main/Front/script.js
@@ -0,0 +1,32 @@
+function limpar() {
+    document.getElementById('nome').value = '';
+    document.getElementById('cpf').value = '';
+    document.getElementById('dtNasc').value = '';
+    document.getElementById('sexo').value = '';
+    document.getElementById('telefone').value = '';
+    document.getElementById('pcd').value = '';
+
+    // COMPOSIÇÃO FAMILIAR
+    document.getElementById('qtdMembros').value = '';
+    document.getElementById('nomeDp').value = '';
+    document.getElementById('dtNascDp').value = '';
+    document.getElementById('parentesco').value = '';
+    document.getElementById('sexoDp').value = '';
+    document.getElementById('pcdDp').value = '';
+    document.getElementById('observacao').value = '';
+
+    // ENDEREÇO
+    document.getElementById('cep').value = '';
+    document.getElementById('rua').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('comp').value = '';
+
+    // Situação residencial
+    document.getElementById('danosResidencia').value = '';
+    document.getElementById('perdasPessoais').value = '';
+    document.getElementById('bensMateriais').value = '';
+    document.getElementById('defesaCivil').value = '';
+
+
+  }
